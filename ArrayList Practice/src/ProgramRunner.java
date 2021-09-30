d2736faaffd805113c2d1accc83dd1e8ab6ab124
Andrew <Andrew@DESKTOP-1MQR1M8.mshome.net>
2021-09-29 23:13:21 -0600

    Finished

--- ArrayList Practice/src/ProgramRunner.java
+++ ArrayList Practice/src/ProgramRunner.java
@@ -2,12 +2,7 @@ import java.util.ArrayList;
 
 public class ProgramRunner {
 
-<<<<<<< HEAD
-	private static ArrayList<Program> tVShows = new ArrayList<Program>();
-	private static ArrayList<String> test = new ArrayList<String>();
-=======
 	static ArrayList<Program> TVShows = new ArrayList<Program>();
->>>>>>> branch 'master' of https://github.com/Andrew-M3373/APCS-ArrayList-Practice.git
 	
 	public static void main(String[] args) {
 
@@ -21,54 +16,20 @@ public class ProgramRunner {
 		printList();
 		
 		
-		test.add("Hello");
-		test.add("world");
-		System.out.println(test.get(0) + test.get(1));
-		
-		
 		
 	}
 	
-<<<<<<< HEAD
-	private static void createArrayList() {
-//		tVShows = new ArrayList<Program>() { {
-//			add(new Program("Star Wars: The Clone Wars", "Science Fiction", 7));
-//			add(new Program("The Mandalorian", "Science Fiction", 2));
-//		}
-//		};
-		
-		
-		
-		tVShows.add(0, new Program("Star Wars: The Clone Wars", "Science Fiction", 7));
-		System.out.println(tVShows.get(0).getTitle());
-		
-		
-		tVShows.add(1,new Program("The Mandalorian", "Science Fiction", 2));
-		System.out.println(tVShows.get(0).getTitle());
-		System.out.println(tVShows.get(1).getTitle());
-
-//		TVShows.add(new Program("Star Wars: Rebels", "Science Fiction", 4));
-//		TVShows.add(new Program("Star Wars: The Bad Batch", "Science Fiction", 1));
-//		TVShows.add(new Program("Disney Gallery: Star Wars: The Mandalorian", "Documentary", 2));
-=======
 	public static void createArrayList() {
 		TVShows.add(new Program("Star Wars: The Clone Wars", "Science Fiction", 12));
 		TVShows.add(new Program("The Mandalorian", "Science Fiction", 2));
 		TVShows.add(new Program("Disney Gallery: Star Wars: The Mandalorian", "Documentary", 2));
 		TVShows.add(new Program("Star Wars: Rebels", "Science Fiction", 4));
 		TVShows.add(new Program("Star Wars: The Bad Batch", "Science Fiction", 1));
->>>>>>> branch 'master' of https://github.com/Andrew-M3373/APCS-ArrayList-Practice.git
 	}
 	
-<<<<<<< HEAD
-	private static void printList() {
-		for (int i = 0; i < tVShows.size(); i++) {
-			System.out.println("\n\n\n\nShow #" + (i+1) + " -- Title: " + tVShows.get(i).getTitle() + "\tGenre: " + tVShows.get(i).getGenre() + "\tSeasons aired: " + tVShows.get(i).getNumberOfSeasonsAired());
-=======
 	public static void printList() {
 		for (int i = 0; i < TVShows.size(); i++) {
 			System.out.println("Show #" + (i+1) + " -- Title: " + TVShows.get(i).getTitle() + "\tGenre: " + TVShows.get(i).getGenre() + "\tSeasons aired: " + TVShows.get(i).getNumberOfSeasonsAired());
->>>>>>> branch 'master' of https://github.com/Andrew-M3373/APCS-ArrayList-Practice.git
 		}
 		System.out.println("\n\n");
 	}
